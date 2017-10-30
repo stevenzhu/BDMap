7ebc574328521291969f9931e3c7fd56a7ec0ec0
zhugongzhao <ab053167zhugongzhao@ab-insurance.com>
2017-10-30 17:24:27 +0800

    SUBMIT

--- app/src/main/java/cn/rongcloud/bdmap/MainActivity.java
+++ app/src/main/java/cn/rongcloud/bdmap/MainActivity.java
@@ -3,10 +3,17 @@ package cn.rongcloud.bdmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.baidu.location.BDLocation;
 import com.baidu.mapapi.SDKInitializer;
+import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.BitmapDescriptor;
+import com.baidu.mapapi.map.BitmapDescriptorFactory;
+import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.map.MyLocationConfiguration;
+import com.baidu.mapapi.map.MyLocationData;
 
 public class MainActivity extends AppCompatActivity {
-
+    private MapView mMapView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -14,5 +21,54 @@ public class MainActivity extends AppCompatActivity {
         //注意该方法要再setContentView方法之前实现
         SDKInitializer.initialize(getApplicationContext());
         setContentView(R.layout.activity_main);
+        //获取地图控件引用
+        mMapView = (MapView) findViewById(R.id.bmapView);
+        BaiduMap mBaiduMap = mMapView.getMap();
+        mBaiduMap.setMapType(BaiduMap.MAP_TYPE_NORMAL);
+//普通地图 ,mBaiduMap是地图控制器对象
+
+//        // 开启定位图层
+//        mBaiduMap.setMyLocationEnabled(true);
+//
+//        //MyLocationConfiguration.LocationMode mCurrentMode = LocationMode.FOLLOWING;//定位跟随态
+//        //MyLocationConfiguration.LocationMode mCurrentMode = LocationMode.NORMAL;   //默认为 LocationMode.NORMAL 普通态
+//        //MyLocationConfiguration.LocationMode mCurrentMode = LocationMode.COMPASS;  //定位罗盘态
+//// 构造定位数据
+//        BDLocation location=new BDLocation();
+//        MyLocationData locData = new MyLocationData.Builder()
+//                .accuracy(location.getRadius())
+//                // 此处设置开发者获取到的方向信息，顺时针0-360
+//                .direction(100).latitude(location.getLatitude())
+//                .longitude(location.getLongitude()).build();
+//
+//// 设置定位数据
+//        mBaiduMap.setMyLocationData(locData);
+//
+//// 设置定位图层的配置（定位模式，是否允许方向信息，用户自定义定位图标）
+//        BitmapDescriptor mCurrentMarker = BitmapDescriptorFactory
+//                .fromResource(R.mipmap.ic_launcher);
+//        MyLocationConfiguration config = new MyLocationConfiguration(MyLocationConfiguration.LocationMode.NORMAL, true, mCurrentMarker);
+//        mBaiduMap.setMyLocationConfiguration(config);
+//
+//// 当不需要定位图层时关闭定位图层
+//        mBaiduMap.setMyLocationEnabled(false);
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //在activity执行onDestroy时执行mMapView.onDestroy()，实现地图生命周期管理
+        mMapView.onDestroy();
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //在activity执行onResume时执行mMapView. onResume ()，实现地图生命周期管理
+        mMapView.onResume();
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //在activity执行onPause时执行mMapView. onPause ()，实现地图生命周期管理
+        mMapView.onPause();
     }
 }
