eaecacd50e5ba4af5499e180075beae8c507c217
stevenzhu <qq1204500708@163.com>
2017-10-30 23:12:45 +0800

    f

--- app/src/main/java/cn/rongcloud/bdmap/MainActivity.java
+++ app/src/main/java/cn/rongcloud/bdmap/MainActivity.java
@@ -2,15 +2,28 @@ package cn.rongcloud.bdmap;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
 
 import com.baidu.location.BDLocation;
 import com.baidu.mapapi.SDKInitializer;
 import com.baidu.mapapi.map.BaiduMap;
 import com.baidu.mapapi.map.BitmapDescriptor;
 import com.baidu.mapapi.map.BitmapDescriptorFactory;
+import com.baidu.mapapi.map.LogoPosition;
 import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.map.MarkerOptions;
 import com.baidu.mapapi.map.MyLocationConfiguration;
 import com.baidu.mapapi.map.MyLocationData;
+import com.baidu.mapapi.map.OverlayOptions;
+import com.baidu.mapapi.map.UiSettings;
+import com.baidu.mapapi.model.LatLng;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
     private MapView mMapView;
@@ -25,6 +38,40 @@ public class MainActivity extends AppCompatActivity {
         mMapView = (MapView) findViewById(R.id.bmapView);
         BaiduMap mBaiduMap = mMapView.getMap();
         mBaiduMap.setMapType(BaiduMap.MAP_TYPE_NORMAL);
+        //定义Maker坐标点
+         List<Map<String,Object>> maps=new ArrayList<Map<String,Object>>();
+        Map<String,Object> map1=new HashMap<String, Object>();
+        map1.put("lat",39.963175f);
+        map1.put("lng",116.400244f);
+        map1.put("url","");
+        Map<String,Object> map2=new HashMap<String,Object>();
+        map2.put("lat",45.963175f);
+        map2.put("lng",100.400244f);
+        map2.put("url","");
+        maps.add(map1);
+        maps.add(map2);
+
+        List<OverlayOptions> overlayOptions=new ArrayList<OverlayOptions>();
+        for(int x=0;x<maps.size();x++){
+            Map<String,Object> map=maps.get(x);
+            LatLng point = new LatLng((float)(map.get("lat")), (float)(map.get("lng")));
+            //构建Marker图标
+            //BitmapDescriptor bitmap = BitmapDescriptorFactory.fromResource(R.mipmap.ic_launcher);
+            View view=LayoutInflater.from(MainActivity.this).inflate(R.layout.bitmapdesriptor,null);
+            BitmapDescriptor bitmap = BitmapDescriptorFactory.fromView(view);
+            //构建MarkerOption，用于在地图上添加Marker
+           // OverlayOptions option = new MarkerOptions().position(point).icon(bitmap);
+            OverlayOptions option=new MarkerOptions().position(point).flat(false).title("aaaa"+x).icon(bitmap);
+            overlayOptions.add(option);
+        }
+        //在地图上添加Marker，并显示
+        mBaiduMap.addOverlays(overlayOptions);
+
+
+        //图Logo
+        //mMapView.setLogoPosition(LogoPosition.logoPostionleftBottom);
+
+
 //普通地图 ,mBaiduMap是地图控制器对象
 
 //        // 开启定位图层
